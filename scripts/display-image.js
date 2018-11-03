1b8ce86895c5a285c60c7ff672299af7090c00e0
Kavelaa <37141924+Kavelaa@users.noreply.github.com>
2018-11-03 21:18:56 +0800

    Vue added#2
    继续用Vue替换了部分原生JS代码，效率提升待考证。

--- scripts/display-image.js
+++ scripts/display-image.js
@@ -1,65 +1,74 @@
-var oldwidth; //Global for ad.offsetWidth
-
-function headerChange() {
-    window.onscroll = function() {    
-        var h1 = document.querySelectorAll('header h1');
-        var ad = document.getElementById('ad');
-        var header = document.getElementsByTagName('header')[0];
-        /*var head = new Vue({
-            el:'header',
-            data:{
-                seen:false
-            },
-            methods:{
-                mouseOver:function() {
-                    this.seen = true;
-                },
-                mouseOut:function() {
-                    this.seen = false;
+var header = new Vue({
+    el:'header',
+    data:{
+        seen:false,
+        flag:true,
+        oldwidth:0,
+        firstFlag:0,
+        adContent:'',
+        headerStyle:{
+            height:0,
+            lineHeight:6 + 'rem',
+            color:'white',
+            backgroundColor:'rgba(255,255,255,0)'
+        },
+        h10Style:{
+            fontSize:4.1+"rem"
+        },
+        h11Style:{
+            fontSize:2.3+'rem'
+        },
+        adStyle:{
+            color:'rgba(255,255,255,0)',
+            height:0
+        },
+    },
+    methods:{
+        mouseOver:function() {
+            this.seen = true;
+        },
+        mouseOut:function() {
+            this.seen = false;
+        },
+        scroll:function() {
+            if(this.flag) {
+                this.flag = false;
+                setTimeout(function(){
+                    this.flag = true;
+                }.bind(this),120);  //限制scroll事件频繁触发回调操作,至于延迟过高导致功能不灵敏的原因不太清楚，经测试，和操作DOM的速度应该有比较大的关系。
+                if (document.documentElement.scrollTop > 150) {
+                    this.h10Style.fontSize = 2 + 'em';
+                    this.h11Style.fontSize = 1.2 + 'em';
+                    this.headerStyle.height = 7 + 'vh';
+                    this.headerStyle.lineHeight = 7 + 'vh';
+                    this.headerStyle.color = 'black';
+                    this.headerStyle.backgroundColor = 'rgba(255,255,255,0.9)';
+                    this.adStyle.backgroundColor = 'rgba(37, 42, 47, 1)';
+                    this.adStyle.height = 100 + '%';
+                    this.adStyle.color = 'rgba(255,255,255,1)';
+                    this.adContent = '云翼计划(享优惠)Supported by Aliyun';
+                    this.oldwidth = this.$refs.adDiv.offsetWidth;
+                    this.firstFlag = 1;
+                }
+                else if (document.body.offsetWidth > 1080) {
+                    this.headerStyle.height = 0;
+                    this.headerStyle.lineHeight = 6 + 'rem';
+                    this.headerStyle.color = 'white';
+                    this.headerStyle.backgroundColor = 'rgba(255,255,255,0)';
+                    this.h10Style.fontSize = 4.1 + 'rem';
+                    this.h11Style.fontSize = 2.3 + 'rem';
+                    this.adStyle.height = 0;
+                    this.adStyle.color = 'rgba(255,255,255,0)';
+                    this.adContent = '';
+                    if(this.firstFlag) this.adStyle.width = this.oldwidth + 'px'; //else if会先于if触发，oldwidth初值会被赋给ad的width属性导致ad宽度始终为0，因此需要先把出现字体的宽度赋给oldwidth之后才能正常运作。
                 }
             }
-        })*/
-        ad.onmouseover = function() {
-            if(document.querySelectorAll('#ad-img').length === 0){
-                var img = document.createElement('img');
-                img.setAttribute('src', 'images/aliyun.png');
-                img.id = 'ad-img';
-                ad.appendChild(img);
-            }
         }
-        ad.onmouseout = function() {
-            var img = document.querySelector('#ad-img');
-            ad.removeChild(img);
-        }
-
-        if (document.documentElement.scrollTop > 150) {
-            h1[0].style.fontSize = 2 + 'em';
-            h1[1].style.fontSize = 1.2 + 'em';
-            header.style.height = 7 + 'vh';
-            header.style.lineHeight = 7 + 'vh';
-            header.style.color = 'black';
-            header.style.backgroundColor = 'rgba(255,255,255,0.9)';
-            ad.style.backgroundColor = 'rgba(37, 42, 47, 1)';
-            ad.style.height = 100 + '%';
-            ad.style.color = 'rgba(255,255,255,1)';
-            ad.firstElementChild.textContent = "云翼计划(享优惠)Supported by Aliyun";
-            oldwidth = ad.offsetWidth;
-        }
-        else if (document.body.offsetWidth > 1080) {
-            header.style.height = 0;
-            header.style.lineHeight = 6 + 'rem';
-            header.style.color = 'white';
-            header.style.backgroundColor = 'rgba(255,255,255,0)';
-            h1[0].style.fontSize = 4.1 + 'rem';
-            h1[1].style.fontSize = 2.3 + 'rem';
-            ad.style.color = 'rgba(37, 42, 47, 0)';
-            ad.style.height = 0;
-            ad.style.color = 'rgba(255,255,255,0)';
-            ad.firstElementChild.textContent = '';
-            ad.style.width = oldwidth + 'px';
-        }
+    },
+    mounted:function() {
+        window.addEventListener('scroll',this.scroll);
     }
-}
+});
 
 function display() {
     var blog = document.getElementById('welcomePage');
@@ -84,6 +93,4 @@ function display() {
         blog.style.maxHeight = 2/3 * blog.offsetWidth + 'px';
     }
 }
-
-headerChange();
 display();
