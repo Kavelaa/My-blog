59c04c05e892a725080798180f18e83ef43c9b2e
Kavelaa <37141924+Kavelaa@users.noreply.github.com>
2018-11-05 21:45:36 +0800

    Vue added#3
    继续进行Vue替换，给banner添加了下拉阴影效果。

--- scripts/display-image.js
+++ scripts/display-image.js
@@ -1,3 +1,4 @@
+//header-----------------------------------------------------------
 var header = new Vue({
     el:'header',
     data:{
@@ -7,16 +8,16 @@ var header = new Vue({
         firstFlag:0,
         adContent:'',
         headerStyle:{
-            height:0,
-            lineHeight:6 + 'rem',
+            /*height:0,
+            lineHeight:6 + 'em',
             color:'white',
-            backgroundColor:'rgba(255,255,255,0)'
+            backgroundColor:'rgba(255,255,255,0)'*/
         },
         h10Style:{
-            fontSize:4.1+"rem"
+            //fontSize:4.1+'rem'
         },
         h11Style:{
-            fontSize:2.3+'rem'
+            //fontSize:2.3+'rem'
         },
         adStyle:{
             color:'rgba(255,255,255,0)',
@@ -35,30 +36,38 @@ var header = new Vue({
                 this.flag = false;
                 setTimeout(function(){
                     this.flag = true;
-                }.bind(this),120);  //限制scroll事件频繁触发回调操作,至于延迟过高导致功能不灵敏的原因不太清楚，经测试，和操作DOM的速度应该有比较大的关系。
+                }.bind(this),100);  //限制scroll事件频繁触发回调操作,至于延迟过高导致功能不灵敏的原因不太清楚，经测试，和操作DOM的速度应该有比较大的关系。
                 if (document.documentElement.scrollTop > 150) {
+                    this.headerStyle = {
+                        height: 7 + 'vh',
+                        lineHeight: 7 + 'vh',
+                        color: 'black',
+                        backgroundColor: 'rgba(255,255,255,0.9)'
+                    }
                     this.h10Style.fontSize = 2 + 'em';
                     this.h11Style.fontSize = 1.2 + 'em';
-                    this.headerStyle.height = 7 + 'vh';
-                    this.headerStyle.lineHeight = 7 + 'vh';
-                    this.headerStyle.color = 'black';
-                    this.headerStyle.backgroundColor = 'rgba(255,255,255,0.9)';
-                    this.adStyle.backgroundColor = 'rgba(37, 42, 47, 1)';
-                    this.adStyle.height = 100 + '%';
-                    this.adStyle.color = 'rgba(255,255,255,1)';
+                    this.adStyle = {
+                        backgroundColor: 'rgba(37, 42, 47, 1)',
+                        height: 100 + '%',
+                        color: 'rgba(255,255,255,1)'
+                    }
                     this.adContent = '云翼计划(享优惠)Supported by Aliyun';
                     this.oldwidth = this.$refs.adDiv.offsetWidth;
                     this.firstFlag = 1;
                 }
                 else if (document.body.offsetWidth > 1080) {
-                    this.headerStyle.height = 0;
-                    this.headerStyle.lineHeight = 6 + 'rem';
-                    this.headerStyle.color = 'white';
-                    this.headerStyle.backgroundColor = 'rgba(255,255,255,0)';
-                    this.h10Style.fontSize = 4.1 + 'rem';
-                    this.h11Style.fontSize = 2.3 + 'rem';
-                    this.adStyle.height = 0;
-                    this.adStyle.color = 'rgba(255,255,255,0)';
+                    this.headerStyle = {
+                        height: 0,
+                        lineHeight: 6 + 'rem',
+                        color: 'white',
+                        backgroundColor: 'rgba(255,255,255,0)'
+                    }
+                    this.h10Style.fontSize = 4.1 + 'em';
+                    this.h11Style.fontSize = 2.3 + 'em';
+                    this.adStyle = {
+                        height: 0,
+                        color: 'rgba(255,255,255,0)'
+                    }
                     this.adContent = '';
                     if(this.firstFlag) this.adStyle.width = this.oldwidth + 'px'; //else if会先于if触发，oldwidth初值会被赋给ad的width属性导致ad宽度始终为0，因此需要先把出现字体的宽度赋给oldwidth之后才能正常运作。
                 }
@@ -70,27 +79,48 @@ var header = new Vue({
     }
 });
 
-function display() {
-    var blog = document.getElementById('welcomePage');
-    var box1 = new Vue({
-        el:'#index-r',
-        data:{
-            seen:false
+//banner------------------------------------------------------------------
+var welcomePage = new Vue({
+    el:'#welcomePage',
+    data:{
+        welcomePageStyle:{
+            boxShadow:String,
         },
-        methods:{
-            mouseOver:function() {
-                this.seen = true;
-            },
-            mouseOut:function() {
-                this.seen = false;
-            }
+    },
+    methods:{
+        scroll:function() {
+            if (document.documentElement.scrollTop > 100)
+                this.welcomePageStyle.boxShadow = 'inset 0 -90vh ' + String(this.shadowValue()*0.8) + 'px rgba(0,0,0,' + String(0.15+this.shadowValue()/900) + ')';
+            else
+                this.welcomePageStyle.boxShadow = ''; 
+        },
+        shadowValue:function() {
+            return (document.documentElement.scrollTop - 100);
         }
-    });
-    window.onload = function() {
-        blog.style.maxHeight = 2/3 * blog.offsetWidth + 'px';
+    },
+    mounted:function() {
+        window.addEventListener('scroll',this.scroll)
+    }
+});
+
+//box1----------------------------------------------------------------
+var box1 = new Vue({
+    el:'#index-r',
+    data:{
+        seen:false
+    },
+    methods:{
+        mouseOver:function() {
+            this.seen = true;
+        },
+        mouseOut:function() {
+            this.seen = false;
+        }
+    }
+});    
+    /*window.onload = function() {
+        welcomePage.style.maxHeight = 2/3 * welcomePage.offsetWidth + 'px';
     }
     window.onresize = function() {
-        blog.style.maxHeight = 2/3 * blog.offsetWidth + 'px';
-    }
-}
-display();
+        welcomePage.style.maxHeight = 2/3 * welcomePage.offsetWidth + 'px';
+    }*/
